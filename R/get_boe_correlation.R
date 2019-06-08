95b39fa176f87f953668a10a87e42b55aadd29af
Joe Busse <joebusse@hotmail.co.uk>
2019-06-08 18:36:19 +0100

    updated get_boe_correlation function

--- R/get_boe_correlation.R
+++ R/get_boe_correlation.R
@@ -2,17 +2,37 @@
 #'
 #' This function calculates the correlation between currency movements in BoE data
 #' @importFrom magrittr %>%
-#' @param date date value, when the forwards are from, default is today's date
-#' @param three_point logical value, whether or not forwards should be presented as three-point estimate (minimum, most likely and maximum value)
+#' @param series a vector of BoE currency codes
+#' @param steps an integer value detailing the number of different correlation intervals to consider
 #' @examples
 #' \dontrun{
-#' get_usd_forward(three_point = FALSE))
-#' get_usd_forward(date = as.Date("2005-01-01"))
+#' get_boe_correlation("xumauss", 12))
 #' }
 #' @export
 #' get_boe_correlation
 
-get_boe_correlation <- function(){
+get_boe_correlation <- function(series, steps){
 
+  currency_series <- get_boe(series)
+
+  add_lags <- function(x){
+
+    df <- currency_series %>%
+      dplyr::group_by(series) %>%
+      dplyr::mutate(lagged_value = dplyr::lag(value, x),
+                    lags = x)
+
+    df
+
+  }
+
+  lagged_currency_series <- purrr::map_dfr(1:steps, add_lags)
+
+  correlation <- lagged_currency_series %>%
+    dplyr::filter(!is.na(lagged_value)) %>%
+    dplyr::group_by(series, lags) %>%
+    dplyr::summarise(correlation = cor(value, lagged_value))
+
+  correlation
 
 }
